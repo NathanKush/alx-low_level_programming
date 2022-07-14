a749c512b756d1a15a8fd9f80f7180d1a9e71e71
NathanKush <makhombenathan@gmail.com>
2022-07-14 18:09:05 +0000

    p

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,19 +5,19 @@
  * @x: string param
  * Return: capitalised version of the string
  */
-char *cap_string(char *x);
+char *cap_string(char *);
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int len = 13;
 	int a = 0, i;
 
-	while (x[a])
+	while (s[a])
 	{
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
-				x[a] = x[a] - 32;
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] = s[a] - 32;
 			i++;
 		}
 		a++;
