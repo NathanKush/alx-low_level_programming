257f837b8f4127c27ce37c436900ab5f7840637f
NathanKush <makhombenathan@gmail.com>
2022-07-18 16:34:37 +0000

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,15 +9,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int size = n;
+	unsigned int a;
 
-	if (size > 0)
-	{
-		int i;
-
-		for (i = 0; i < size; i++)
-			dest[i] = src[i];
-	}
-
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
 	return (dest);
 }
