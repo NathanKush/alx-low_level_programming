ce4adfb2e6d38443596139387eb5ac09cbd1c42c
NathanKush <makhombenathan@gmail.com>
2022-07-18 10:24:19 +0000

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of sring at first occurence of the whole substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0; j, x;
+
+	if (needle[0] == '\0'
+		return (haystack);
+
+	while (haystack[i] != '\0')
+	{
+		if (haaystack[i] == needle[0])
+		{
+			x = i, j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == need;e[j])
+					x++, j++;
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
