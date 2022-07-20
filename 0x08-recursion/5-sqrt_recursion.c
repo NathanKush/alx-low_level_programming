af56abd9635d394c8ce1e770c733c042e78eaeb7
NathanKush <makhombenathan@gmail.com>
2022-07-20 15:59:24 +0000

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * find_root - find square root of n, starting from the smallest possible, 0
+ * _sqrt_recursion - find square root of n, starting from the smallest possible, 0
  * @n: n
  * @root: test this root
  * Return: natural square root, or -1 if not natural root
