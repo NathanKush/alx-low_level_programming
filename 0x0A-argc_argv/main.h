5fea4b0efa817940b825902ccdc838ccae619c36
NathanKush <makhombenathan@gmail.com>
2022-07-22 19:17:56 +0000

    3

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,3 +2,4 @@ int main(int argc, char *argv[]);
 int _putchar(char c);
 int main(int argc, char *argv[] _attribute_((unused)));
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
